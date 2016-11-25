d953b0486cd563ec0a7d2fab1fb291cf2329ae69
NathzZPersia <nathzzpersia@gmail.com>
2016-11-25 16:10:38 -0400

    firts commit

---
+++ DB_PROJECT/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is the main.
+		System.out.println("This is the main hello");
+	}
+
+}
